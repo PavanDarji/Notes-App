3343ca92cf92cda9e503f4c93eef32ac409ce03f
Pavan Darji <84569753+PavanDarji@users.noreply.github.com>
2022-02-09 18:53:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ function showNotes() {
                       <div class="card-body">
                           <h5 class="card-title">${element.title}</h5>
                           <p class="card-text">  ${element.text}</p>
-                          <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                          <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-danger">Delete Note</button>
                       </div>
                   </div>`;
     });
